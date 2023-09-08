835352557521d211b008d027dbe6c25435a40fa1
Gianluca Mazzini <gianluca@mazzini.org>
2023-09-08 07:42:13 +0200

    Create utilizzo_parcheggi.php

---
+++ utilizzo_parcheggi.php
@@ -0,0 +1,20 @@
+<?php
+  function mydata($a,$b){
+    $aa=substr($a,4,2).substr($a,2,2).substr($a,0,2);
+    $bb=substr($b,4,2).substr($b,2,2).substr($b,0,2);
+    return strcmp($aa,$bb);
+  }
+
+  $fp=fopen("w1.csv","r");
+  for(;;){
+    $aux=fgets($fp);
+    if(feof($fp))break;
+    $dd=explode(",",$aux);
+    @ $acc[$dd[0]][substr($dd[2],0,1)]++;
+  }
+  fclose($fp);
+  uksort($acc,"mydata");
+  foreach($acc as $k => $v){
+    @ printf("%s,%d,%d\n",$k,$acc[$k]["B"],$acc[$k]["L"]);
+  }
+?>
