5b005fd9cb275472a01f3ea1007164d5c6d5b04a
Gianluca Mazzini <gianluca@mazzini.org>
2023-09-08 09:18:11 +0200

    Add files via upload

---
+++ parametro_z.php
@@ -0,0 +1,32 @@
+<?php
+  function mydata($a,$b){
+    $aa=substr($a,4,2).substr($a,2,2).substr($a,0,2);
+    $bb=substr($b,4,2).substr($b,2,2).substr($b,0,2);
+    return strcmp($aa,$bb);
+  }
+
+  $fp=fopen("r2.csv","r");
+  for(;;){
+    $aux=fgets($fp);
+    if(feof($fp))break;
+    $dd=explode(",",$aux);
+    $rr=explode(":",$dd[3]);
+    $oo=$rr[0]*60+$rr[1];
+    @ $acc[$dd[1]][$dd[0]]+=$oo;
+  }
+  fclose($fp);
+
+  $fp=fopen("r1.csv","r");
+  for(;;){
+    $aux=fgets($fp);
+    if(feof($fp))break;
+    $dd=explode(",",$aux);
+    if($dd[2]=="1")unset($acc[$dd[1]][$dd[0]]);
+  }
+  fclose($fp);
+
+  ksort($acc);
+  foreach($acc as $k => $v){
+    printf("%s,%d\n",$k,count($v));
+  }
+?>
