ebf3b8536a2f7a5c4980743014e12b6b08577b0d
Gianluca Mazzini <gianluca@mazzini.org>
2023-11-16 10:07:54 +0100

    Update minuti_lavorati.php

--- minuti_lavorati.php
+++ minuti_lavorati.php
@@ -9,6 +9,7 @@ $fp=fopen("ore.csv","r");
 for(;;){
   $aux=fgets($fp);
   if(feof($fp))break;
+  if(str_contains($aux,"\""))continue;
   $dd=explode(",",trim($aux));
   $rr=explode(":",$dd[3]);
   $oo=$rr[0]*60+$rr[1];
