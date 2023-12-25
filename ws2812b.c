ea519a9c7f911dc1b335815aca614e91d2e58574
Gianluca Mazzini <gianluca@mazzini.org>
2023-12-25 23:54:34 +0100

    Update ws2812b.c

--- ws2812b.c
+++ ws2812b.c
@@ -2,5 +2,14 @@
 #incluse <stdio.h>
 
 void main(){
-  struct timespec ts;
+  struct timespec t0h,t1h,t0l,t1l;
+  t0l.tv_sec=0; t0l.tv_nsec=850;
+  t0h.tv_sec=0; t0h.tv_nsec=400;
+  t1l.tv_sec=0; t1l.tv_nsec=450;
+  t1h.tv_sec=0; t1h.tv_nsec=800;
+
+  nanosleep(&t0h,NULL); nanosleep(&t0l,NULL);
+  nanosleep(&t1h,NULL); nanosleep(&t1l,NULL);
+
+  
 }
