929e05a4a8e9b0476d8100b1cedff6e92f9f7b63
Gianluca Mazzini <gianluca@mazzini.org>
2024-01-16 07:16:30 +0100

    Create bgp4.php

---
+++ fulltable/bgp4.php
@@ -0,0 +1,17 @@
+<?php
+
+$o1=file_get_contents("/home/www/fulltable.mazzini.org/ip4.txt");
+eval('$ip4='.$o1.';');
+
+uksort($ip4,"mycmp");
+$fp=fopen("/home/www/fulltable.mazzini.org/m4.txt","w");
+foreach($ip4 as $k => $v)fprintf($fp,"%s,%d\n",$k,$v);
+fclose($fp);
+
+function mycmp($a,$b){
+  $ma=explode(".",$a); $va=($ma[2])|($ma[1]<<8)|($ma[0]<<16);
+  $mb=explode(".",$b); $vb=($mb[2])|($mb[1]<<8)|($mb[0]<<16);
+  return $va-$vb;
+}
+
+?>
