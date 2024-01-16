89567ab953cdf391c97166a57eb64aeb23431101
Gianluca Mazzini <gianluca@mazzini.org>
2024-01-16 07:18:02 +0100

    Create out6.php

---
+++ fulltable/out6.php
@@ -0,0 +1,24 @@
+<?php
+
+$o1=file_get_contents("/home/www/fulltable.mazzini.org/ip6.txt");
+eval('$ip6='.$o1.';');
+
+uksort($ip6,"mycmp");
+$fp=fopen("/home/www/fulltable.mazzini.org/m6.txt","w");
+foreach($ip6 as $k => $v)fprintf($fp,"%s,%d\n",$k,$v);
+fclose($fp);
+
+function mycmp($a,$b){
+  $ma=explode(":",$a);
+  $mb=explode(":",$b);
+  for($i=0;$i<6;$i++){
+    if(isset($ma[$i]))$va=hexdec($ma[$i]); else $va=0;
+    if(isset($mb[$i]))$vb=hexdec($mb[$i]); else $vb=0;
+    if($va==$vb)continue;
+    if($va>$vb)return 1;
+    else return -1;
+  }
+  return 0;
+}
+
+?>
