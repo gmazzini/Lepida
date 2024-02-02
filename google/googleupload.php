daa3918d18ac2e05b2929018d27448682234f453
Gianluca Mazzini <gianluca@mazzini.org>
2024-02-02 23:21:22 +0100

    Update googleupload.php

--- google/googleupload.php
+++ google/googleupload.php
@@ -2,29 +2,15 @@
 include "googleset.php";
 $access_token=file_get_contents("/home/www/data/access_token");
 
-
-
-DA FINIREEEE
-
-
-
-$curlPost='
-{
-  "valueInputOption": "RAW",
-  "data": [
-    {
-      "range": "Sheet1!A1:A2",
-      "majorDimen
-  ]
-}';
-
+$file_content=file_get_contents($target_file); 
+$mime_type=mime_content_type($target_file); 
 $ch = curl_init();
 curl_setopt($ch,CURLOPT_URL,"https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart");
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
 curl_setopt($ch,CURLOPT_POST,1);
 curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
-curl_setopt($ch,CURLOPT_HTTPHEADER,Array("Content-Type: application/json","Authorization: Bearer ".$access_token));
-curl_setopt($ch,CURLOPT_POSTFIELDS,$curlPost);
+curl_setopt($ch,CURLOPT_HTTPHEADER,Array("Content-Type: ".$mime_type,"Authorization: Bearer ".$access_token));
+curl_setopt($ch,CURLOPT_POSTFIELDS,$file_content);
 echo curl_exec($ch);
 echo "\n";
 curl_close($ch);
