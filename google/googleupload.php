518aa7ea876a8263de3839d813af293f7184a7e0
Gianluca Mazzini <gianluca@mazzini.org>
2024-02-03 07:53:54 +0100

    Update googleupload.php

--- google/googleupload.php
+++ google/googleupload.php
@@ -2,7 +2,8 @@
 $access_token=file_get_contents("/mybind/counted/access_token");
 $target_file="/mybind/counted/19753";
 $file_content=file_get_contents($target_file); 
-$mime_type=mime_content_type($target_file); 
+$mime_type=mime_content_type($target_file);
+
 $ch=curl_init();
 curl_setopt($ch,CURLOPT_URL,"https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart");
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
@@ -10,7 +11,21 @@ curl_setopt($ch,CURLOPT_POST,1);
 curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
 curl_setopt($ch,CURLOPT_HTTPHEADER,Array("Content-Type: ".$mime_type,"Authorization: Bearer ".$access_token));
 curl_setopt($ch,CURLOPT_POSTFIELDS,$file_content);
+$oo=json_decode(curl_exec($ch),true);
+print_r($oo);
+curl_close($ch);
+
+$file_id=$oo["id"];
+$ch=curl_init();
+curl_setopt($ch,CURLOPT_URL,"https://www.googleapis.com/drive/v3/files/$file_id");
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch,CURLOPT_POST,1);
+curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
+curl_setopt($ch,CURLOPT_HTTPHEADER,array("Content-Type: application/json","Authorization: Bearer ".$access_token));
+curl_setopt($ch,CURLOPT_CUSTOMREQUEST,"PATCH");
+curl_setopt($ch,CURLOPT_POSTFIELDS,'{"name": "pippo.csv"}');
 echo curl_exec($ch);
 echo "\n";
 curl_close($ch);
+
 ?>
