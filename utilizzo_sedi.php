8d445b9501ebd32310ba51dea1d57affd9b6f35b
Gianluca Mazzini <gianluca@mazzini.org>
2024-03-12 17:43:35 +0100

    Create utilizzo_sedi.php

---
+++ utilizzo_sedi.php
@@ -0,0 +1,14 @@
+<?php
+$fp=fopen("prenotazioni.csv","r");
+for(;;){
+  $aux=fgets($fp);
+  if(feof($fp))break;
+  $dd=explode(",",trim($aux));
+  @ $acc[$dd[0]][substr($dd[2],0,3)]++;
+}
+fclose($fp);
+
+uksort($acc,"mydata");
+foreach($acc as $k => $v){
+  @ printf("%s,%d\n",$k,$v);
+}
