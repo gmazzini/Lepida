87f4e66103e7bffe3df0fe2beb0efe5946cfe402
Gianluca Mazzini <gianluca@mazzini.org>
2025-01-29 06:33:50 +0100

    Update formazione_per_persona.php

--- formazione_per_persona.php
+++ formazione_per_persona.php
@@ -25,4 +25,24 @@ foreach($lines as $aux){
 }
 foreach($acc as $k => $v)printf("%s,%d\n",$k,$v);
 
+$access_token=file_get_contents("/home/www/data/access_token");
+$curlPost='
+{
+  "valueInputOption": "RAW",
+  "data": [{
+      "range": "f_ore!A1",
+      "majorDimension": "ROWS",
+      "values": [['ciao']]
+    }]
+}';
+$ch=curl_init();
+curl_setopt($ch,CURLOPT_URL,"https://sheets.googleapis.com/v4/spreadsheets/1vNh5kuo0xzQxOpPIiiUSCoG7csWNu1IFL4PPa4-t_Vo/values:batchUpdate");
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch,CURLOPT_POST,1);
+curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
+curl_setopt($ch,CURLOPT_HTTPHEADER,Array("Content-Type: application/json","Authorization: Bearer ".$access_token));
+curl_setopt($ch,CURLOPT_POSTFIELDS,$curlPost);
+curl_exec($ch);
+curl_close($ch);
+
 ?>
