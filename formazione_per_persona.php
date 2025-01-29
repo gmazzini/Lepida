b2dc8ec759882f1b66cde7b4e9ab6b3a4d499048
Gianluca Mazzini <gianluca@mazzini.org>
2025-01-29 06:20:19 +0100

    Update formazione_per_persona.php

--- formazione_per_persona.php
+++ formazione_per_persona.php
@@ -10,8 +10,8 @@ for(;;){
 fclose($fp);
 
 $acc=array();
-$from=date(0101y);
-$to=date(3112y);
+$from=date("0101y");
+$to=date("3112y");
 
 $all=file_get_contents("https://orelavorate.lepida.it/download/proc.php?token=$token&from=$from&to=$to");
 $lines=explode("\n",$all);
