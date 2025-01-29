1dbe08529e953b822b99e89c22fe214faddaba8d
Gianluca Mazzini <gianluca@mazzini.org>
2025-01-29 06:19:30 +0100

    Update formazione_per_persona.php

--- formazione_per_persona.php
+++ formazione_per_persona.php
@@ -1,4 +1,5 @@
 <?php
+include "token.php";
 
 $fp=fopen("formazione","r");
 for(;;){
@@ -9,10 +10,12 @@ for(;;){
 fclose($fp);
 
 $acc=array();
-$fp=fopen("ore.csv","r");
-for(;;){
-  $aux=fgets($fp);
-  if(feof($fp))break;
+$from=date(0101y);
+$to=date(3112y);
+
+$all=file_get_contents("https://orelavorate.lepida.it/download/proc.php?token=$token&from=$from&to=$to");
+$lines=explode("\n",$all);
+foreach($lines as $aux){
   if(str_contains($aux,"\""))continue;
   $dd=explode(",",trim($aux));
   if(!isset($act[$dd[2]]))continue;
@@ -20,7 +23,6 @@ for(;;){
   $oo=$rr[0]*60+$rr[1];
   @$acc[$dd[1]]+=$oo;
 }
-fclose($fp);
 foreach($acc as $k => $v)printf("%s,%d\n",$k,$v);
 
 ?>
